9d17ef7c5b6abe29b9797215843932e4cb9285af
akankshaH <43901836+akankshaH@users.noreply.github.com>
2018-10-25 21:28:06 +0530

    Add files via upload

--- minesweeper.c
+++ minesweeper.c
@@ -14,23 +14,36 @@ int process(char msweep[6][6],int r,int c,char userboard[6][6]);
 int main()
 {
 setbuf(stdout, NULL);
+int a=1,pts=0;
+do
+{
+
 char msweep[6][6] = {{'0'}};
 int i,r,c;
 char userboard[6][6] = {{'0'}};
 rand_mines(msweep);
-printmine(msweep,6,userboard);
+
+do
+{
+	 i=0;
+	 printmine(msweep,6,userboard);
 printf("Enter location(ONLY 1 - 5) on the mine field (row, column)\n");
-  scanf("%d%d",&r,&c);
+ scanf("%d%d",&r,&c);
 i = process(msweep,r,c,userboard);
- while(i == 1)
-   {
+	 int e=0,d=0;pts=0;
+   for(e=1;e<6;e++)
+       {
+   	  for(d=1;d<6;d++)
+         {
+          if (msweep[e][d]!=0)
+          {pts++; }
+         }
+    	}
  printf(" %c Surrounding MINES\n\n",msweep[r][c]);
-printmine(msweep,6,userboard);
-printf("enter next location...(ONLY 1 - 5) ");
-     scanf("%d%d",&r,&c);
-     i=0;
-i = process(msweep,r,c,userboard);
- }
+ if(pts==25)
+ {printf("YOU WON! \n");break;}
+ } while(i == 1);
+
 if(i==0)
   { printf("GAME OVER \n");
   int i,j;
@@ -43,17 +56,23 @@ if(i==0)
      printf("\n\n");
  	}
   }
+
+printf("DO YOU WANT TO PLAY AGAIN?[1 for yes and 0 for no]");
+scanf("%d",&a);
+}while(a!=0);
 return 0;
 }
+
 void rand_mines(char msweep[6][6])
 {
+int randomnos[5]={1,2,3,4,5};
 int r,c,m;
 for(m=0;m<15;m++)
 {
- r = rand() % 6;
-c = rand() % 6;
+ r = randomnos[rand() % 5];
+c =  randomnos[rand() % 5];
 msweep[r][c] = '*';
- // printf("%d %d \n",r,c); /*check or cheat*/
+ //printf("%d %d \n",r,c); /*check or cheat*/
  }
 return;
 }
